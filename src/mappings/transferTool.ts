0f1927c9a8960382453c508c1039df911b576f06
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-07-07 16:12:58 -0300

    feat: add code to handle transfer tools

---
+++ src/mappings/transferTool.ts
@@ -0,0 +1,88 @@
+// - event: L2WalletOwnerSet(indexed address,indexed address)
+// handler: handleL2WalletOwnerSet
+// - event: LockedFundsSentToL2(indexed address,indexed address,indexed address,address,uint256)
+// handler: handleLockedFundsSentToL2
+// - event: L2WalletAddressSet(indexed address,indexed address)
+// handler: handleL2WalletAddressSet
+// - event: ETHDeposited(indexed address,uint256)
+// handler: handleETHDeposited
+// - event: ETHWithdrawn(indexed address,indexed address,uint256)
+// handler: handleETHWithdrawn
+// - event: ETHPulled(indexed address,uint256)
+// handler: handleETHPulled
+import { BigInt, log } from "@graphprotocol/graph-ts";
+import {
+  L2WalletOwnerSet,
+  LockedFundsSentToL2,
+  L2WalletAddressSet,
+  ETHDeposited,
+  ETHWithdrawn,
+  ETHPulled
+} from "../types/L1GraphTokenLockTransferTool/L1GraphTokenLockTransferTool";
+import {
+  L1GraphTokenLockTransferToolSet
+} from "../types/L1Staking/L1Staking";
+import {
+  L1GraphTokenLockTransferTool
+} from "../types/templates";
+
+import { TokenLockWallet } from "../types/schema";
+
+export function handleL2WalletOwnerSet(event: L2WalletOwnerSet): void {
+  let tokenLockWallet = TokenLockWallet.load(event.params.l1WalletOwner.toHexString())!;
+  tokenLockWallet.l2Beneficiary = event.params.l2WalletOwner;
+  tokenLockWallet.save();
+}
+
+export function handleLockedFundsSentToL2(event: LockedFundsSentToL2): void {
+  let tokenLockWallet = TokenLockWallet.load(event.params.l1Wallet.toHexString())!;
+
+  if(!tokenLockWallet.transferredToL2) {
+    tokenLockWallet.transferredToL2 = true
+    tokenLockWallet.firstTransferredToL2At = event.block.timestamp
+    tokenLockWallet.firstTransferredToL2AtBlockNumber = event.block.number
+    tokenLockWallet.firstTransferredToL2AtTx = event.transaction.hash.toHexString()
+  }
+  tokenLockWallet.lastTransferredToL2At = event.block.timestamp
+  tokenLockWallet.lastTransferredToL2AtBlockNumber = event.block.number
+  tokenLockWallet.lastTransferredToL2AtTx = event.transaction.hash.toHexString()
+  tokenLockWallet.tokensTransferredToL2 = tokenLockWallet.tokensTransferredToL2.plus(
+    event.params.amount
+  );
+  tokenLockWallet.save();
+}
+
+export function handleL2WalletAddressSet(event: L2WalletAddressSet): void {
+  let tokenLockWallet = TokenLockWallet.load(event.params.l1Wallet.toHexString())!;
+  tokenLockWallet.l2WalletAddress = event.params.l2Wallet;
+  tokenLockWallet.save();
+}
+
+export function handleETHDeposited(event: ETHDeposited): void {
+  let tokenLockWallet = TokenLockWallet.load(event.params.tokenLock.toHexString())!;
+  tokenLockWallet.ethBalance = tokenLockWallet.ethBalance.plus(
+    event.params.amount
+  );
+  tokenLockWallet.save();
+}
+
+export function handleETHWithdrawn(event: ETHWithdrawn): void {
+  let tokenLockWallet = TokenLockWallet.load(event.params.tokenLock.toHexString())!;
+  tokenLockWallet.ethBalance = tokenLockWallet.ethBalance.minus(
+    event.params.amount
+  );
+  tokenLockWallet.save();
+}
+
+export function handleETHPulled(event: ETHPulled): void {
+  let tokenLockWallet = TokenLockWallet.load(event.params.tokenLock.toHexString())!;
+  tokenLockWallet.ethBalance = tokenLockWallet.ethBalance.minus(
+    event.params.amount
+  );
+  tokenLockWallet.save();
+}
+
+export function handleL1GraphTokenLockTransferToolSet(event: L1GraphTokenLockTransferToolSet): void {
+  L1GraphTokenLockTransferTool.create(event.params.l1GraphTokenLockTransferTool)
+}
+
